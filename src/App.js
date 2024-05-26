f26f12a0aff7344a29d1f4fd0898c4763ad424b0
DineshNehra29 <156710996+DineshNehra29@users.noreply.github.com>
2024-05-26 09:15:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './style.css';
+import './style.css'
+import { AiOutlineDelete , AiOutlineEdit} from 'react-icons/ai';
+import { BsCheckLg } from 'react-icons/bs';
+
+
+
 
 function App() {
   const [newTitle, setNewTitle] = useState('');
@@ -40,42 +45,69 @@ function App() {
   return (
     <>
       <h2>Hello</h2>
-      <form className='new-form' onSubmit={addItem}>
-        <div className='form-row'>
-          <label htmlFor='item'>New Item</label>
-          <input
-            type='text'
-            id='item'
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
-          />
-          <label htmlFor='description'>Description</label>
-          <input
-            type='text'
-            id='description'
-            value={newDescription}
-            onChange={(e) => setNewDescription(e.target.value)}
-          />
-          <button className='btn' type='submit'>
-            Add
-          </button>
-        </div>
-      </form>
-      <h1 className='header'>Todo List</h1>
-      <ul className='list'>
-        {Todo.map((item, index) => (
-          <li key={index}>
-            <label>
-              <input type='checkbox' />
-              {item.title}
-            </label>
-            <p>{item.description}</p>
-            <button className='del' onClick={() => handleDelete(index)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="todo-wrapper">
+        <form className='new-form' onSubmit={addItem}>
+          <div className="todo-input">
+            <div className="todo-input-item">
+              <label>Title</label>
+              <input
+                type="text"
+                value={newTitle}
+                onChange={e => setNewTitle(e.target.value)}
+                placeholder="What's the task title?"
+              />
+            </div>
+            <div className="todo-input-item">
+              <label>Description</label>
+              <input
+                type="text"
+                value={newDescription}
+                onChange={e => setNewDescription(e.target.value)}
+                placeholder="What's the task description?"
+              />
+            </div>
+            <div className="todo-input-item">
+              <button
+                type="submit"
+
+                className="primaryBtn"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
+        <h1 className='header'>Todo List</h1>
+        <ul className='list'>
+          {Todo.map((item, index) => (
+            <li key={index}>
+              <div className="todo-list-item" >
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div>
+                  <AiOutlineDelete
+                    className="icon"
+                    onClick={() => handleDelete(index)}
+                    title="Delete?"
+                  />
+                  <BsCheckLg
+                    className="check-icon"
+
+                    title="Complete?"
+                  />
+                  <AiOutlineEdit className="edit"
+
+                    title="Edit?" />
+                </div>
+
+              </div>
+
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
